452f53b7d6609ebf0e12d298b729e754ab607146
ponomyev <ponomyev@fel.cvut.cz>
2018-12-26 18:28:57 +0100

    working on exprs

--- symcalc/expr.cpp
+++ symcalc/expr.cpp
@@ -2,6 +2,39 @@
 #include "expr_impl.hpp"
 #include <stdexcept>
 
+
+
+exprs::variable::variable(std::string in) {var = in;};
+
+expr exprs::variable::derive(std::string const &variable) const {
+    if (var == variable){
+        return expr::ONE;
+    }else{
+        return expr::ZERO;
+    }
+}
+bool exprs::variable::equals(const expr_base &b) const {
+    if(const variable* v = dynamic_cast<variable const*>(b.shared_from_this().get())) {
+        return v->var == var;
+    }
+    return false;
+}
+void exprs::variable::write(std::ostream &out, expr_base::WriteFormat fmt) const {
+    out << var;
+}
+//TODO: check this shieeet, this could produce an error
+double exprs::variable::evaluate(const expr_base::variable_map_t &variables) const {
+    for(auto pair : variables){
+        if(pair.first == var){
+            return pair.second;
+        }
+    }
+    throw unbound_variable_exception("");
+}
+expr exprs::variable::simplify() const {
+    return shared_from_this();
+}
+
 const expr expr::ZERO = expr::number(0.0);
 const expr expr::ONE = expr::number(1.0);
 
--- symcalc/expr_impl.cpp
+++ symcalc/expr_impl.cpp
@@ -5,61 +5,27 @@
 #include <limits>
 
 namespace exprs {
+    exprs::number::number(double number) {num = number;};
 
-
-
-    number::number(double number) {num = number;};
-
-    expr number::derive(std::string const &variable) const {
+    expr exprs::number::derive(std::string const &variable) const {
         return expr::ZERO;
     }
-    bool number::equals(const expr_base &b) const {
+    bool exprs::number::equals(const expr_base &b) const {
         if(const number* v = dynamic_cast<number const*>(b.shared_from_this().get())) {
             return v->num == num;
         }
         return false;
     }
-    void number::write(std::ostream &out, expr_base::WriteFormat fmt) const {
+    void exprs::number::write(std::ostream &out, expr_base::WriteFormat fmt) const {
         out << num;
     }
-    double number::evaluate(const expr_base::variable_map_t &variables) const {
+    double exprs::number::evaluate(const expr_base::variable_map_t &variables) const {
         return num;
     }
-    expr number::simplify() const {
+    expr exprs::number::simplify() const {
         return shared_from_this();
     }
-
-    variable::variable(std::string in) {var = in;};
-
-    expr variable::derive(std::string const &variable) const {
-        if (var == variable){
-            return expr::ONE;
-        }else{
-            return expr::ZERO;
-        }
-    }
-    bool variable::equals(const expr_base &b) const {
-        if(const variable* v = dynamic_cast<variable const*>(b.shared_from_this().get())) {
-            return v->var == var;
-        }
-        return false;
-    }
-    void variable::write(std::ostream &out, expr_base::WriteFormat fmt) const {
-        out << var;
-    }
-    //TODO: check this shieeet, this could produce an error
-    double variable::evaluate(const expr_base::variable_map_t &variables) const {
-       for(auto pair : variables){
-           if(pair.first == var){
-               return pair.second;
-           }
-       }
-       throw unbound_variable_exception("");
-    }
-    expr variable::simplify() const {
-        return shared_from_this();
-    }
-
+    //PLUS
     expr_plus::expr_plus(const expr & one, const expr & two) {
         expr1 = one;
         expr2 = two;
@@ -68,7 +34,74 @@ namespace exprs {
         return std::make_shared<expr_plus>(expr_plus(expr1->derive(variable), expr2->derive(variable)));
     }
     bool expr_plus::equals(const expr_base &b) const {
-        if (b.)
+        //get the actual expression from shared pointer
+        const expr_plus* expr = dynamic_cast<expr_plus const*>(b.shared_from_this().get());
+        if (expr1 == expr->expr1 && expr2 == expr->expr2){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    void expr_plus::write(std::ostream &out, expr_base::WriteFormat fmt) const {
+        if(fmt == expr_base::WriteFormat::Infix){
+            out << "(" << expr1 << " + " << expr2 << ")";
+        } else if(fmt == expr_base::WriteFormat::Postfix){
+            out << "(";
+            out << expr1;
+            out << " ";
+            out << expr2;
+            out << " +)";
+        } else {
+            out << "(+ " << expr1 << " " << expr2 << ")";
+        }
+    }
+    double expr_plus::evaluate(const expr_base::variable_map_t &variables) const {
+        return expr1->evaluate(variables) + expr2->evaluate(variables);
+    }
+    expr expr_plus::simplify() const {
+       expr one = expr1->simplify();
+       expr two = expr2->simplify();
+
+       if (one == expr::ZERO){
+           return two;
+       }
+       if(two == expr::ONE){
+           return one;
+       }
+       return std::make_shared<expr_plus>(expr_plus(one,two));
+    }
+    //MINUS
+    expr_minus::expr_minus(const expr &one, const expr &two) {
+        expr1 = one;
+        expr2 = two;
+    }
+    expr expr_minus::derive(std::string const &variable) const {
+        return std::make_shared<expr_minus>(expr_minus(expr1->derive(variable), expr2->derive(variable)));
+    }
+    void expr_minus::write(std::ostream &out, expr_base::WriteFormat fmt) const {
+        if(fmt == expr_base::WriteFormat::Infix){
+            out << "(" << expr1 << " - " << expr2 << ")";
+        } else if(fmt == expr_base::WriteFormat::Postfix){
+            out << "(";
+            out << expr1;
+            out << " ";
+            out << expr2;
+            out << " -)";
+        } else {
+            out << "(- " << expr1 << " " << expr2 << ")";
+        }
+    }
+    double expr_minus::evaluate(const expr_base::variable_map_t &variables) const {
+        return expr1->evaluate(variables) - expr2->evaluate(variables);
+    }
+    expr expr_minus::simplify() const {
+        expr one = expr1->simplify();
+        expr two = expr2->simplify();
+
+        if(two == expr::ONE){
+            return one;
+        }
+        return std::make_shared<expr_minus>(expr_minus(one,two));
     }
 
 } // namespace exprs
